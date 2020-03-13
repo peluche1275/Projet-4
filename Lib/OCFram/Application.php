74470c34e8536f6691f693796731ae75ba048787
Nathan <nathan.altomare.belier@gmail.com>
2020-03-13 12:28:03 +0100

    Update Application.php , add BackController, Mangers and PDOFactory

--- Lib/OCFram/Application.php
+++ Lib/OCFram/Application.php
@@ -4,66 +4,71 @@ namespace OCFram;
 
 abstract class Application
 {
-    protected $httpRequest;
-    protected $httpResponse;
-    protected $name;
+  protected $httpRequest;
+  protected $httpResponse;
+  protected $name;
 
-    public function __construct()
+  public function __construct()
+  {
+    $this->httpRequest = new HTTPRequest($this);
+    $this->httpResponse = new HTTPResponse($this);
+
+    $this->name = '';
+  }
+
+  public function getController()
+  {
+    $router = new Router;
+
+    $xml = new \DOMDocument;
+    $xml->load(__DIR__.'/../../App/'.$this->name.'/Config/routes.xml');
+
+    $routes = $xml->getElementsByTagName('route');
+
+    foreach ($routes as $route)
     {
-        $this->httpRequest = new HTTPRequest($this);
-        $this->httpResponse = new HTTPResponse($this);
+      $vars = [];
 
-        $this->name = '';
+      if ($route->hasAttribute('vars'))
+      {
+        $vars = explode(',', $route->getAttribute('vars'));
+      }
+
+      $router->addRoute(new Route($route->getAttribute('url'), $route->getAttribute('module'), $route->getAttribute('action'), $vars));
     }
 
-    public function getController()
+    try
     {
-        $router = new Router;
-
-        $xml = new \DOMDocument;
-        $xml->load(__DIR__ . '/../../App/' . $this->name . '/Config/routes.xml');
-
-        $routes = $xml->getElementsByTagName('route');
-
-        foreach ($routes as $route) {
-            $vars = [];
-
-            if ($route->hasAttribute('vars')) {
-                $vars = explode(',', $route->getAttribute('vars'));
-            }
-            $router->addRoute(new Route($route->getAttribute('url'), $route->getAttribute('module'), $route->getAttribute('action'), $vars));
-        }
-
-        try {
-            $matchedRoute = $router->getRoute($this->httpRequest->requestURI());
-        } catch (\RuntimeException $e) {
-            if ($e->getCode() == Router::NO_ROUTE) {
-
-                $this->httpResponse->redirect404();
-            }
-        }
-
-        $_GET = array_merge($_GET, $matchedRoute->vars());
-
-
-        $controllerClass = 'App\\' . $this->name . '\\Modules\\' . $matchedRoute->module() . '\\' . $matchedRoute->module() . 'Controller';
-        return new $controllerClass($this, $matchedRoute->module(), $matchedRoute->action());
+      $matchedRoute = $router->getRoute($this->httpRequest->requestURI());
+    }
+    catch (\RuntimeException $e)
+    {
+      if ($e->getCode() == Router::NO_ROUTE)
+      {
+        $this->httpResponse->redirect404();
+      }
     }
 
-    abstract public function run();
+    $_GET = array_merge($_GET, $matchedRoute->vars());
 
-    public function httpRequest()
-    {
-        return $this->httpRequest;
-    }
+    $controllerClass = 'App\\'.$this->name.'\\Modules\\'.$matchedRoute->module().'\\'.$matchedRoute->module().'Controller';
+    return new $controllerClass($this, $matchedRoute->module(), $matchedRoute->action());
+  }
 
-    public function httpResponse()
-    {
-        return $this->httpResponse;
-    }
+  abstract public function run();
 
-    public function name()
-    {
-        return $this->name;
-    }
+  public function httpRequest()
+  {
+    return $this->httpRequest;
+  }
+
+  public function httpResponse()
+  {
+    return $this->httpResponse;
+  }
+
+  public function name()
+  {
+    return $this->name;
+  }
 }
